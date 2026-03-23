faa5230fee709272bf6b4d1182c7530c03c189ec
IAmKami03 <wrldsage@gmail.com>
2026-03-23 11:50:05 +0100

    changed brevo client

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,14 +1,11 @@
-const SibApiV3Sdk = require("sib-api-v3-sdk");
+const { BrevoClient } = require("@getbrevo/brevo");
 
-const client = SibApiV3Sdk.ApiClient.instance;
-const apiKey = client.authentications["api-key"];
-
-apiKey.apiKey = process.env.BREVO_API_KEY;
-
-const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+const client = new BrevoClient({
+  apiKey: process.env.BREVO_API_KEY,
+});
 
 exports.sendEmail = async (to, subject, htmlContent) => {
-  await tranEmailApi.sendTransacEmail({
+  await client.transactionalEmails.sendTransacEmail({
     sender: { email: "wrldsage@gmail.com", name: "Fony App" },
     to: [{ email: to }],
     subject,
